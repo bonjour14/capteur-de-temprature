59e99afb52e1c01f139106a4243fcdadf9a913a3
bonjour14 <103410186+bonjour14@users.noreply.github.com>
2022-05-09 09:21:14 +0200

    Fichiers initiaux pour un projet MakeCode

---
+++ main.ts
@@ -0,0 +1,27 @@
+input.onButtonPressed(Button.A, function () {
+    basic.showString("la température actuelle du processeur")
+    basic.showNumber(min)
+    basic.clearScreen()
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showNumber(max)
+    basic.clearScreen()
+})
+let min = 0
+let max = 0
+let currenttemperature = input.temperature()
+max = currenttemperature
+min = currenttemperature
+basic.forever(function () {
+    basic.showString(".")
+    currenttemperature = input.temperature()
+    if (currenttemperature < min) {
+        min = currenttemperature
+    }
+    if (currenttemperature > max) {
+        max = currenttemperature
+    }
+    basic.pause(1000)
+    basic.clearScreen()
+    basic.pause(1000)
+})
